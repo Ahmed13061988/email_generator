119e933632dba8890a97a0c3c69072e03098c8ff
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-20 23:06:09 -0600

    Sending email with articles title and the description.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import plistlib
 import requests
+from send_email import send_email
 
 api_key = "2ca2e4fc76d6437ba40dae617fe02fab"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-21&sortBy=publishedAt" \
@@ -8,8 +9,13 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=2024-07-21&sortBy=publishe
 request = requests.get(url)
 content = request.json()
 
+body = ""
 
-for i in content["articles"]:
-    print(i)
+for article in content["articles"]:
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
 
+body = body.encode("utf-8")
+
+send_email(body)
 
